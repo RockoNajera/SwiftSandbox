19bafe15e453e5d72665d5507f18072b5ed933aa
Adriana Marin <adriannamariin@gmail.com>
2021-08-04 14:56:17 -0500

    data types and switch statement were declared on the view controller

--- SwiftSandbox/SwiftSandbox/ViewController.swift
+++ SwiftSandbox/SwiftSandbox/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var weekDays: Int = 0
 
+    switch weekDays {
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    default:
+        print("Error")
+
+    }
+    
+    
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
 
+    var age: Int = 27
+    var message: String = "Aloha"
+    var weight: Double = 70.550
+    var length: Float = 1.52
+    var names: [String] = ["Antonio", "Arnulfo", "Amelia"]
+    var id: [Int: Any] = [2748: "Serrano", 1017: "Arteaga", 1618: "Ramos"]
+    var vocal: Character = "A"
+    var months: 
+    
+    
+    
 
-}
+   
+    
+    
+    
 
+    }
+   
